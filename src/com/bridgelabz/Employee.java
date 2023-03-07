ee0758fd9ee3b9d2b2506952264183d8d654b397
sanjana8100 <sanjanams812@gmail.com>
2023-03-07 19:37:47 +0530

    added: UC2

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -4,12 +4,20 @@ public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
         System.out.println("Checking whether Employee is present or absent...");
-        int empPresent = 1;
+        int IsPresent = 1;
+        int EmpWagePerHour = 20;
+        int empHrs, empWage;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == empPresent){
+        if(empCheck == IsPresent){
             System.out.println("Employee is Present");
+            empHrs=8;
         }
-        else
+        else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
+        }
+        empWage = empHrs * EmpWagePerHour;
+        System.out.println("Employee Hours: "+empHrs);
+        System.out.println("Employee Wage: "+empWage);
     }
 }
