08db37d65a8ea16653841454257e3bdc89ac6b98
sanjana8100 <sanjanams812@gmail.com>
2023-03-07 19:41:08 +0530

    added: UC3

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -4,13 +4,18 @@ public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
         System.out.println("Checking whether Employee is present or absent...");
-        int IsPresent = 1;
+        int IsFullTime = 1;
+        int IsPartTime = 2;
         int EmpWagePerHour = 20;
         int empHrs, empWage;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IsPresent){
-            System.out.println("Employee is Present");
+        if(empCheck == IsFullTime){
+            System.out.println("Employee is working Full Time.");
             empHrs=8;
+        }
+        else if(empCheck == IsPartTime){
+            System.out.println("Employee is working Part Time.");
+            empHrs=4;
         }
         else {
             System.out.println("Employee is Absent");
