1fa592d45178b0cfacc91bcd216fbd9dee1c1d58
sanjana8100 <sanjanams812@gmail.com>
2023-03-07 19:34:18 +0530

    added: UC1

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -3,5 +3,13 @@ package com.bridgelabz;
 public class Employee {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
+        System.out.println("Checking whether Employee is present or absent...");
+        int empPresent = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == empPresent){
+            System.out.println("Employee is Present");
+        }
+        else
+            System.out.println("Employee is Absent");
     }
 }
