0db3b88e30adff71fb4040cea9b5d8b8d6e81f99
sanjana8100 <sanjanams812@gmail.com>
2023-03-07 19:44:04 +0530

    added: UC4

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -1,25 +1,28 @@
 package com.bridgelabz;
 
 public class Employee {
+    public static final int IsFullTime = 1;
+    public static final int IsPartTime = 2;
+    public static final int EmpWagePerHour = 20;
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
-        System.out.println("Checking whether Employee is present or absent...");
-        int IsFullTime = 1;
-        int IsPartTime = 2;
-        int EmpWagePerHour = 20;
+
         int empHrs, empWage;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IsFullTime){
-            System.out.println("Employee is working Full Time.");
-            empHrs=8;
-        }
-        else if(empCheck == IsPartTime){
-            System.out.println("Employee is working Part Time.");
-            empHrs=4;
-        }
-        else {
-            System.out.println("Employee is Absent");
-            empHrs = 0;
+        switch ((int) empCheck) {
+            case IsFullTime:
+                System.out.println("Employee is Doing Full Time:");
+                empHrs = 8;
+                break;
+            case IsPartTime:
+                System.out.println("Employee is Doing Part Time:");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent:");
+                empHrs = 0;
+                break;
         }
         empWage = empHrs * EmpWagePerHour;
         System.out.println("Employee Hours: "+empHrs);
