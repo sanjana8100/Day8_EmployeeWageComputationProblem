cb70669d3753b987ddf4dc15b3ce023775bf27f8
sanjana8100 <sanjanams812@gmail.com>
2023-03-07 19:46:39 +0530

    added: UC5

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -4,28 +4,33 @@ public class Employee {
     public static final int IsFullTime = 1;
     public static final int IsPartTime = 2;
     public static final int EmpWagePerHour = 20;
+    public static final int WorkingDaysPerMonth = 20;
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program On Master Branch");
 
-        int empHrs, empWage;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        switch ((int) empCheck) {
-            case IsFullTime:
-                System.out.println("Employee is Doing Full Time:");
-                empHrs = 8;
-                break;
-            case IsPartTime:
-                System.out.println("Employee is Doing Part Time:");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent:");
-                empHrs = 0;
-                break;
+        int empHrs, empWagePerDay, empWagePerMonth = 0;
+        for(int day = 0 ; day<=WorkingDaysPerMonth ; day++) {
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+            switch ((int) empCheck) {
+                case IsFullTime:
+                    System.out.println("Employee is Doing Full Time:");
+                    empHrs = 8;
+                    break;
+                case IsPartTime:
+                    System.out.println("Employee is Doing Part Time:");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent:");
+                    empHrs = 0;
+                    break;
+            }
+            empWagePerDay = empHrs * EmpWagePerHour;
+            empWagePerMonth += empWagePerDay;
+            System.out.println("Employee Hours: " + empHrs);
+            System.out.println("Employee Wage per Day: " + empWagePerDay);
         }
-        empWage = empHrs * EmpWagePerHour;
-        System.out.println("Employee Hours: "+empHrs);
-        System.out.println("Employee Wage: "+empWage);
+        System.out.println("Emp Wage Per Month : "+empWagePerMonth);
     }
 }
